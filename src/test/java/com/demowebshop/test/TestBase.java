66efc1b6a699e29cd4ded18e2838376d4d854e12
cohen <cohen0402@gmail.com>
2025-03-19 23:10:23 +0200

    HW3

--- src/test/java/com/demowebshop/test/TestBase.java
+++ src/test/java/com/demowebshop/test/TestBase.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
@@ -14,14 +15,17 @@ public class TestBase {
 @BeforeMethod
     public void setUp(){
     driver = new ChromeDriver();
-    driver.get("https://telranedu.web.app/home");
+    driver.get("https://demowebshop.tricentis.com/");
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 }
 
-@AfterMethod
+@AfterMethod (enabled = false)
     public void tearDown(){
     driver.quit();
 }
+
+
+
 
 }
