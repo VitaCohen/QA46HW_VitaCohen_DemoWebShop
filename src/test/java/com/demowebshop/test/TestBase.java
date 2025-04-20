f60deedd911421a958d0fc784966763b876468bf
cohen <cohen0402@gmail.com>
2025-04-20 22:45:00 +0300

    HW 4 HW 6  Value Object model + task browser HW 7 suites + logs

--- src/test/java/com/demowebshop/test/TestBase.java
+++ src/test/java/com/demowebshop/test/TestBase.java
@@ -2,12 +2,18 @@ package com.demowebshop.test;
 
 import com.demowebshop.fw.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.lang.reflect.Method;
+
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser",
             Browser.CHROME.browserName()));
@@ -25,5 +31,13 @@ public class TestBase {
         app.stop();
     }
 
+    @BeforeMethod
+    public void startTest(Method method){
+        logger.info("Start test " + method.getName());
+    }
+    @AfterMethod
+    public void stopTest(Method method){
+        logger.info("Stop test " + method.getName());
+    }
 
 }
