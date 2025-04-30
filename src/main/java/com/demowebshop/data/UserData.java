c84b4196b887484ad9bc842e68cf6b4620924d7b
cohen <cohen0402@gmail.com>
2025-04-30 23:09:47 +0300

    HW 4 HW 6  Value Object model + task browser HW 7 suites + HW8 logs+ screenshots HW 9 DataProvider

--- src/main/java/com/demowebshop/data/UserData.java
+++ src/main/java/com/demowebshop/data/UserData.java
@@ -4,6 +4,6 @@ public class UserData {
 
     public static final String NAME="Viky";
     public static final String LASTNAME="Vays";
-    public static final String EMAIL="vik1321@gmail.com";
+    public static final String EMAIL="vik050589@gmail.com";
     public static final String PASSWORD="123456Aa!";
 }
