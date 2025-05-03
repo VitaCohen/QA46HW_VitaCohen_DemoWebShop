34670ba19eda34cb6c237cde5c661d30ccf79240
cohen <cohen0402@gmail.com>
2025-05-03 22:12:38 +0300

    HW 4 HW 6  Value Object model + task browser HW 7 suites + HW8 logs+ screenshots HW 9 DataProvider

--- src/main/java/com/demowebshop/data/UserData.java
+++ src/main/java/com/demowebshop/data/UserData.java
@@ -4,6 +4,6 @@ public class UserData {
 
     public static final String NAME="Viky";
     public static final String LASTNAME="Vays";
-    public static final String EMAIL="vik9872@gmail.com";
+    public static final String EMAIL="vik98721@gmail.com";
     public static final String PASSWORD="123456Aa!";
 }
--- src/main/java/com/demowebshop/utils/DataProviders.java
+++ src/main/java/com/demowebshop/utils/DataProviders.java
@@ -18,9 +18,9 @@ public class DataProviders {
     @DataProvider
     public Iterator<Object[]> addNewUser() {
         List<Object[]> list = new ArrayList<>();
-        list.add(new Object[]{"Oliver", "Kaz", "o1288@gmail.com", "13458Aa!"});
-        list.add(new Object[]{"Orly", "Kan", "o81289@gmail.com", "23458Aa!"});
-        list.add(new Object[]{"Hen", "Omar", "h71288o@gmail.com", "33458Aa!"});
+        list.add(new Object[]{"Oliver", "Kaz", "o11288@gmail.com", "13458Aa!"});
+        list.add(new Object[]{"Orly", "Kan", "o811289@gmail.com", "23458Aa!"});
+        list.add(new Object[]{"Hen", "Omar", "h711288o@gmail.com", "33458Aa!"});
         return list.iterator();
     }
 
